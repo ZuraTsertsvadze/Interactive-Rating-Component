6927b268909be7529925b582da54a55856f102ee
soso <totorotomoney@gmail.com>
2023-01-10 22:28:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const value = document.querySelector(".value");
 const numButton = document.querySelectorAll(".num");
@@ -8,62 +8,27 @@ const number = document.querySelector(".num");
 const thanksBody = document.querySelector(".ty-body");
 
 numButton.forEach((el) => {
-
-
-    el.addEventListener("click", (e) => {
-
-        const reseter = function () {
-
-            numButton.forEach((el) => {
-
-                el.style.backgroundColor = "#363f4b";
-                el.classList.remove("active");
-            });
-        };
-
-        reseter();
-
-
-        const rateadder = function () {
-            el.style.backgroundColor = "#959EAC";
-            el.style.color = "white";
-            value.innerHTML = parseInt(el.innerHTML);
-            el.classList.add("active");
-
-        };
-
-        rateadder();
-
+  el.addEventListener("click", (e) => {
+    numButton.forEach((el) => {
+      el.style.backgroundColor = "#363f4b";
+      el.classList.remove("active");
     });
 
+    el.style.backgroundColor = "#959EAC";
+    el.style.color = "white";
+    value.innerHTML = parseInt(el.innerHTML);
+    el.classList.add("active");
+  });
 });
 
+// const rateShower = () => {
+submit.addEventListener("click", (e) => {
+  numButton.forEach((el) => {
+    if (el.classList.contains("active")) {
+      thanksBody.style.zIndex = "1";
+    }
+  });
+});
+// };
 
-
-const rateShower = function () {
-
-
-    submit.addEventListener("click", (e) => {
-
-
-        numButton.forEach((el) => {
-
-            if (el.classList.contains("active")) {
-
-                thanksBody.style.zIndex = "1";
-
-            } else {
-                return
-            };
-
-
-        });
-
-
-
-
-    });
-
-};
-
-rateShower();
+// rateShower();
